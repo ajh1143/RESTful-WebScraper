e47e1762bc5435d095781e2dd2b0edeb1a13bc26
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-05-02 22:57:43 -0700

    Update REST-Server.js

--- REST-Server.js
+++ REST-Server.js
@@ -1,2 +1,13 @@
+//Express 
 var express = require('express');
 var app = express();
+
+//Route
+app.get('/', function (req, res) {
+   res.send('Testing');
+});
+
+// Port Listening
+app.listen(5000, function () {
+    console.log('Listening to port: 5000');
+});
